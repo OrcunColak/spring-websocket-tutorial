e701ca34db47b737a4f7f6597326e8957fce751f
Orçun Çolak <orcun.colak@hazelcast.com>
2024-03-21 09:27:56 +0300

    example to get queryParams

--- src/main/java/com/colak/springwebsockettutorial/config/WebSocketConfig.java
+++ src/main/java/com/colak/springwebsockettutorial/config/WebSocketConfig.java
@@ -1,6 +1,7 @@
 package com.colak.springwebsockettutorial.config;
 
 import com.colak.springwebsockettutorial.handler.SocketTextHandler;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
@@ -8,11 +9,15 @@ import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry
 
 @Configuration
 @EnableWebSocket
+
+@RequiredArgsConstructor
 public class WebSocketConfig implements WebSocketConfigurer {
+
+    private final SocketTextHandler socketTextHandler;
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
         // http://localhost:8080/index.html
-        registry.addHandler(new SocketTextHandler(), "/user");
+        registry.addHandler(socketTextHandler, "/user");
     }
 }
--- src/main/java/com/colak/springwebsockettutorial/handler/SocketTextHandler.java
+++ src/main/java/com/colak/springwebsockettutorial/handler/SocketTextHandler.java
@@ -1,20 +1,38 @@
 package com.colak.springwebsockettutorial.handler;
 
+import lombok.extern.slf4j.Slf4j;
 import org.json.JSONObject;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.io.IOException;
+import java.net.URI;
+import java.util.Map;
 
 @Component
+@Slf4j
 public class SocketTextHandler extends TextWebSocketHandler {
 
     @Override
     public void handleTextMessage(WebSocketSession session, TextMessage message) throws IOException {
+        logQueryParams(session);
+
         String payload = message.getPayload();
         JSONObject jsonObject = new JSONObject(payload);
         session.sendMessage(new TextMessage("Hi " + jsonObject.get("user") + " how may we help you?"));
+    }
+
+    // See https://msmechatronics.medium.com/websocket-wizardry-spring-webflux-edition-ca9d30346465
+    private void logQueryParams(WebSocketSession session) {
+        URI uri = session.getUri();
+        if (uri != null) {
+            UriComponentsBuilder builder = UriComponentsBuilder.fromUri(uri);
+
+            Map<String, String> queryParams = builder.build().getQueryParams().toSingleValueMap();
+            log.info("QueryParams : {}", queryParams);
+        }
     }
 }
